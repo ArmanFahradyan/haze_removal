2299081eddf83331160f196c33103f6e07fd0c45
ArmanFahradyan <arman.fahradyan@ispras.ru>
2024-04-02 14:54:46 +0400

    added results

--- compute_metric.py
+++ compute_metric.py
@@ -1,11 +1,14 @@
 from metrics import entropy_for_dir
+import os
 
 # source_dir = "datasets/small_data"
-source_dir = "results/small_data/haze_removal"
+source_dir = "results/Distributed_haze1k/test_thick/single_Image_Dehazing/"
 # destination_file = "metric_scores/small_data/original_images.txt"
-destination_file = "metric_scores/small_data/haze_removal.txt"
+destination_file = "metric_scores/Distributed_haze1k/test_thick/single_Image_Dehazing.txt"
 
 def main():
+    destination_path = destination_file[:destination_file.rfind('/')]
+    os.makedirs(destination_path, exist_ok=True)
     avg_entropy = entropy_for_dir(source_dir, destination_file)
     print(f"avg entropy:\t{avg_entropy}")
 
--- process_dehazing.py
+++ process_dehazing.py
@@ -5,8 +5,8 @@ from single_image_dehazing import image_dehazer
 from haze_removal import run_file
 
 
-source_dir = "datasets/small_data"
-destination_dir = "results/small_data/haze_removal"
+source_dir = "datasets/Distributed_haze1k/test_thick/input"
+destination_dir = "results/Distributed_haze1k/test_thick/single_Image_Dehazing" # haze_removal
 # destination_dir = "results/small_data/Single_Image_Dehazing"
 
 
@@ -15,9 +15,9 @@ def main():
         os.makedirs(destination_dir, exist_ok=True)
     image_names = sorted([file for file in os.listdir(source_dir) if not file.startswith('.')])
     for name in image_names:
-        # image = cv2.imread(os.path.join(source_dir, name))
-        # processed_image, haze_map = image_dehazer.remove_haze(image, showHazeTransmissionMap=False)
-        processed_image = run_file(os.path.join(source_dir, name))
+        image = cv2.imread(os.path.join(source_dir, name))
+        processed_image, haze_map = image_dehazer.remove_haze(image, showHazeTransmissionMap=False)
+        # processed_image = run_file(os.path.join(source_dir, name))
         cv2.imwrite(os.path.join(destination_dir, name), processed_image)
 
 
