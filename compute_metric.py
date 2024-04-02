7e82829090c682017c1c72e22ffaf129698f29f7
ArmanFahradyan <arman.fahradyan@ispras.ru>
2024-04-02 14:22:42 +0400

    changed gitignore and some pathes in compute_metrics

--- compute_metric.py
+++ compute_metric.py
@@ -1,7 +1,9 @@
 from metrics import entropy_for_dir
 
-source_dir = "datasets/small_data"
-destination_file = ""
+# source_dir = "datasets/small_data"
+source_dir = "results/small_data/haze_removal"
+# destination_file = "metric_scores/small_data/original_images.txt"
+destination_file = "metric_scores/small_data/haze_removal.txt"
 
 def main():
     avg_entropy = entropy_for_dir(source_dir, destination_file)
